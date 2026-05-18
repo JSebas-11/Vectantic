54bcb3e1cdba1c41dc24a502e80c65167228a2e8
JSebas11 <juans3.jsda@gmail.com>
2026-05-17 22:55:41 -0500

    SemanticSearchService now includes support for AboveThreshold searching

--- Vectantic.Semantic/src/Internal/Services/SemanticSearchService.cs
+++ Vectantic.Semantic/src/Internal/Services/SemanticSearchService.cs
@@ -15,39 +15,45 @@ internal sealed class SemanticSearchService : ISemanticSearchService {
 
     // -------------------- METHS --------------------
     public async Task<SemanticSearchResults> SearchAsync(string query, IReadOnlyList<string> docs, int topK) {
-        var queryTask = _embeddingService.EmbedAsync(query);
-        var docsTask = _embeddingService.EmbedBatchAsync(docs);
-        await Task.WhenAll(queryTask, docsTask).ConfigureAwait(false);
-
-        var topKList = TryTopK(queryTask.Result.Vector, [.. docsTask.Result.Select(e => e.Vector)], topK);
-
-        var matches = new List<SemanticMatch>(topK);
-        foreach (var (index, score) in topKList)
-            matches.Add(new SemanticMatch() 
-                { Text = docs[index], OriginalIndex = index, Score = score }
-            );
-
-        return new SemanticSearchResults() { Matches = matches, TotalCandidates = docs.Count };
-    }
-
-    public Task<SemanticSearchResults> SearchAsync(float[] query, IReadOnlyList<float[]> docs, int topK) {
-        var topKList = TryTopK(query, docs, topK);
-
-        var matches = new List<SemanticMatch>(topK);
-        foreach (var (index, score) in topKList)
-            matches.Add(new SemanticMatch() 
-                { OriginalIndex = index, Score = score }
-            );
-
-        return Task.FromResult(new SemanticSearchResults() 
-            { Matches = matches, TotalCandidates = docs.Count }
+        var (queryEmbed, docsEmbed) = await GetEmbeddingsAsync(query, docs);
+        var topKList = TryMath(
+            () => EmbeddingMath.TopK(queryEmbed, docsEmbed, topK)
         );
+
+        return SemanticResultDoc(topKList, docs);
     }
+
+    public async Task<SemanticSearchResults> SearchAsync(string query, IReadOnlyList<string> docs, float minScore) {
+        var (queryEmbed, docsEmbed) = await GetEmbeddingsAsync(query, docs);
+        var minScoreList = TryMath(
+            () => EmbeddingMath.AboveThreshold(queryEmbed, docsEmbed, minScore)
+        );
+
+        return SemanticResultDoc(minScoreList, docs);
+    }
+
+    public Task<SemanticSearchResults> SearchAsync(float[] query, IReadOnlyList<float[]> docs, int topK)
+        => Task.FromResult(
+                SemanticResultVector(
+                    TryMath(() => EmbeddingMath.TopK(query.AsSpan(), docs, topK)),
+                    docs.Count
+                )
+            );
+
+    public Task<SemanticSearchResults> SearchAsync(float[] query, IReadOnlyList<float[]> docs, float minScore)
+        => Task.FromResult(
+                SemanticResultVector(
+                    TryMath(() => EmbeddingMath.AboveThreshold(query.AsSpan(), docs, minScore)),
+                    docs.Count
+                )
+            );
 
     // -------------------- INNER METHS --------------------
-    private static IReadOnlyList<(int index, float score)> TryTopK(float[] query, IReadOnlyList<float[]> docs, int topK) {
+    private static IReadOnlyList<(int index, float score)> TryMath(
+        Func<IReadOnlyList<(int index, float score)>> func) 
+    {
         try {
-            return EmbeddingMath.TopK(query.AsSpan(), docs, topK);
+            return func();
         }
         catch (ArgumentOutOfRangeException ex) {
             throw new VectanticMathException($"A math error occurred during semantic searching: {ex.Message}", ex);
@@ -55,5 +61,40 @@ internal sealed class SemanticSearchService : ISemanticSearchService {
         catch (ArgumentException ex) {
             throw new VectanticMathException($"A math error occurred during semantic searching: {ex.Message}", ex);
         }
+    }
+
+    private async Task<(float[] queryEmbed, IReadOnlyList<float[]> docsEmbed)> GetEmbeddingsAsync(
+        string query, IReadOnlyList<string> docs) 
+    {
+        var queryTask = _embeddingService.EmbedAsync(query);
+        var docsTask = _embeddingService.EmbedBatchAsync(docs);
+        await Task.WhenAll(queryTask, docsTask).ConfigureAwait(false);
+
+        var docsVectors = docsTask.Result.Select(e => e.Vector).ToArray();
+
+        return (queryTask.Result.Vector, docsVectors.AsReadOnly());
+    }
+
+    private static SemanticSearchResults SemanticResultVector(
+        IReadOnlyList<(int index, float score)> matchesList, int totalDocs) 
+    {
+        var matches = new List<SemanticMatch>(matchesList.Count);
+        foreach (var (index, score) in matchesList)
+            matches.Add(new SemanticMatch() 
+                { OriginalIndex = index, Score = score }
+            );
+
+        return new SemanticSearchResults() { Matches = matches, TotalCandidates = totalDocs } ;
+    }
+    private static SemanticSearchResults SemanticResultDoc(
+        IReadOnlyList<(int index, float score)> matchesList, IReadOnlyList<string> docs) 
+    {
+        var matches = new List<SemanticMatch>(matchesList.Count);
+        foreach (var (index, score) in matchesList)
+            matches.Add(new SemanticMatch() 
+                { Text = docs[index], OriginalIndex = index, Score = score }
+            );
+
+        return new SemanticSearchResults() { Matches = matches, TotalCandidates = docs.Count };
     }
 }
